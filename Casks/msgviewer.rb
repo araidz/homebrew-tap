f69f4df64ef70de850023460d2419017214ef22d
araidz <riadm.ahmed@gmail.com>
2026-07-26 09:47:13 +0400

    msgviewer 0.1.7

--- Casks/msgviewer.rb
+++ Casks/msgviewer.rb
@@ -1,6 +1,6 @@
 cask "msgviewer" do
-  version "0.1.6"
-  sha256 "c95736ebcfd3effe829d9212dd502ed6360b640424b1da3de024e63fe2d93b78"
+  version "0.1.7"
+  sha256 "9cae2d6f5752cb89f1c1953cea11e59d87a9126cda5db2fbf519f4417b06f456"
 
   url "https://github.com/araidz/MSGViewer/releases/download/v#{version}/MSGViewer.app.zip"
   name "MSG Viewer"
